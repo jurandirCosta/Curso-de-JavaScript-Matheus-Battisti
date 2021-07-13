dbf078e1f17a91b8492666f71663a3fdff02b69b
Jurandir da Costa <juracosta1410@gmail.com>
2021-07-13 10:52:58 -0300

    Segundo commit das lições

---
+++ 6_string/js/scripts.js
@@ -0,0 +1,22 @@
+var nome = "Jurandir";
+var sobrenome = "da Costa";
+
+var nomeCompleto = nome + " " + sobrenome;
+
+console.log(nome);
+console.log(typeof nome);
+
+console.log(nomeCompleto);
+
+console.log(typeof "5.292929");
+
+var frase = 'Esta é uma frase complexa';
+
+console.log(frase);
+console.log(typeof frase);
+
+console.log(nome + " " + frase);
+
+document.write("Olá"); // document.white não é mais permitido no javascript
+
+
