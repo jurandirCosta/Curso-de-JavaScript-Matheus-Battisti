69e1bb8a51ec6c7be29a4bded6f5c30b045c2f06
Jurandir da Costa <juracosta1410@gmail.com>
2021-07-13 08:52:07 -0300

    Primeiro commit das lições

---
+++ 3_particularidades_js/js/scripts.js
@@ -0,0 +1,23 @@
+console.log("Primeiro");
+
+console.log("Último");
+
+console.log("Mais um console.log");
+
+
+// tipagem fraca
+var a = 1;
+
+a = 'teste';
+
+console.log(a);
+
+
+// Este é um comentário
+
+/*
+Este 
+é
+um 
+comentário
+*/
