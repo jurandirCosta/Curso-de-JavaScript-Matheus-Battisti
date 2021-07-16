790853b6c4192ef6945ad14e7409f60ba06cb455
Jurandir da Costa <juracosta1410@gmail.com>
2021-07-16 08:54:22 -0300

    Terceiro commit das lições

---
+++ 10_arrays/js/scripts.js
@@ -0,0 +1,19 @@
+var arr = [5, "Matheus", true, {teste: 1, teste: 2}]; // obj => {}
+
+console.log(arr);
+
+var arr2 = [2,3,4,5,6];
+
+console.log(arr2);
+
+console.log(arr[1]);
+
+console.log(arr2[0]);
+
+arr.push(10); //Adiciona elementos no array
+
+arr[6] = 20; //Adiciona elementos no array
+
+arr[0] = "Teste"; // modifica elementos no array
+
+console.log(arr);
---
+++ 8_null_undefined/js/scripts.js
@@ -0,0 +1,14 @@
+// Sempre criar as variáveis no inicio
+// ou seja antes delas serem usadas,
+// se não terá um undefined
+
+var nome = null;
+console.log(nome);
+nome = "Jurandir";
+console.log(nome);
+
+var sobrenome;
+var sobrenomme = "Costa";
+
+console.log(sobrenome);
+console.log(sobrenomme);
